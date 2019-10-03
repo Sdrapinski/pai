c9db904e84230ab16f5df8cf98ceebbcdcc7c76e
Sdrapinski <55097897+Sdrapinski@users.noreply.github.com>
2019-10-03 18:19:02 +0200

    Add files via upload

---
+++ dolaczanie_pliku.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+
+  poczatek pliku
+
+<?php
+/*
+include './name.php'; //dolaczanie pliku
+*/
+require './name.php';
+
+ ?>
+ koniec pliku
+</body>
+</html>
